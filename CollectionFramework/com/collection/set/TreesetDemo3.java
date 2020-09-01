e29db028c69a815f3b2dea212ccf1d44357c7f10
rohit <kumbharrohit13@gmail.com>
2020-09-01 17:05:42 +0530

    package name changed

---
+++ CollectionFramework/com/collection/set/TreesetDemo3.java
@@ -0,0 +1,14 @@
+package com.collection.set;
+
+import java.util.TreeSet;
+
+public class TreesetDemo3
+{
+	public static void main(String[] args)
+	{
+		System.out.println("A".compareTo("Z"));
+		System.out.println("Z".compareTo("K"));
+		System.out.println("A".compareTo("A"));
+		System.out.println("A".compareTo(null));
+	}
+}
