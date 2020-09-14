d97ed4bac8e5a8b375cb84e268df6b225dfe0fe4
rohit <kumbharrohit13@gmail.com>
2020-09-14 10:32:53 +0530

    Cursor packages redesigned

--- CollectionFramework/collection/cursor/EnumerationTest.java
+++ CollectionFramework/collection/cursor/EnumerationTest.java
@@ -1,4 +1,4 @@
-package com.collection.cursor;
+package collection.cursor;
 import java.util.Enumeration;
 import java.util.Vector;
 
--- CollectionFramework/collection/cursor/IteratorDemo.java
+++ CollectionFramework/collection/cursor/IteratorDemo.java
@@ -1,4 +1,4 @@
-package com.collection.cursor;
+package collection.cursor;
 
 import java.util.ArrayList;
 import java.util.Iterator;
